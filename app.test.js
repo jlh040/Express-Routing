7c86ce7ca211617619cf14aab6e3ebcc280aeeef
Jeff Hall <jeff.leon.hall@gmail.com>
2021-05-23 19:51:05 -0700

    wrote a test to check the errors

--- app.test.js
+++ app.test.js
@@ -8,8 +8,24 @@ describe('mean tests', () => {
     })
 
     test('are we getting accurate answers?', async () => {
-        const resp = await request(app).get('/mean?nums=5,6,7')
-        let respObj = JSON.parse(resp.text)
-        expect(respObj['response']['value']).toEqual(6)
+        const resp = await request(app).get('/mean?nums=5,6,7');
+        let respObj = JSON.parse(resp.text);
+        expect(respObj['response']['value']).toEqual(6);
+
+        const resp2 = await request(app).get('/mean?nums=8,8,8');
+        let respObj2 = JSON.parse(resp2.text);
+        expect(respObj2['response']['value']).toEqual(8);;
+    })
+
+    test('do our errors work?', async () => {
+        const resp = await request(app).get('/mean');
+        let respObj = JSON.parse(resp.text);
+        expect(respObj['Error']).toContain('Nums are required')
+
+        const resp2 = await request(app).get('/mean?nums=1,5,7,dog');
+        let respObj2 = JSON.parse(resp2.text);
+        expect(respObj2['Error']).toContain('dog is not a number')
+
+
     })
 });
