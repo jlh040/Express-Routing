0e20754bac2cfe2ccaa663a9f5a04a9649d1d829
Jeff Hall <jeff.leon.hall@gmail.com>
2021-05-23 18:49:33 -0700

    changed testing strategy from axios to supertest

--- app.test.js
+++ app.test.js
@@ -1,9 +1,1 @@
-const axios = require('axios');
-
-describe('mean tests', () => {
-    test('does the response have a 200 status code?', async () => {
-        let resp = await axios.get('http://localhost:3000/mean');
-        expect(1).toBe(1);
-        console.log(resp);
-    })
-})
+const request = require('supertest');
