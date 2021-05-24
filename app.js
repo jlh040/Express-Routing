12cb3ce3cea3070546da14fd7142ad6c35858513
Jeff Hall <jeff.leon.hall@gmail.com>
2021-05-23 19:09:01 -0700

    minor change

--- app.js
+++ app.js
@@ -39,10 +39,6 @@ app.use((err, req, res, next) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
-})
-
 module.exports = {
     app
 }
--- app.test.js
+++ app.test.js
@@ -1,9 +1,9 @@
-// const request = require('supertest');
-// const { app } = require('./app');
+const request = require('supertest');
+const { app } = require('./app');
 
-// describe('mean tests', () => {
-//     test('do we get a 200 status code?', async () => {
-//         const response = await request(app).get('/mean?nums=1,2,3');
-//         expect(response.status).toEqual(200);
-//     })
-// })
+describe('mean tests', () => {
+    test('do we get a 200 status code?', async () => {
+        const response = await request(app).get('/mean?nums=1,2,3');
+        expect(response.status).toEqual(200);
+    })
+})
